4168393fae9dda39bbdf29ce54b4db7a514ac2ef
bigsegs <cw.plastering.services@gmail.com>
2023-06-06 01:01:20 +0100

    header to mobile optimised

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,18 @@
  export const Header=()=>{
     return(
         <div id="header">
-        <div className="sides" id="tel">
-         <p> tel: 07597 852 695</p>
-        
-        </div>
+       
         <div id="chris">
              <h1>Chris Wilson</h1>  
        <h2>Renovations and property maintenance</h2>
-       <h3 id="boro">Peterborough / Cambridgeshire</h3>
+       <h3 id="boro">Peterborough  Cambridgeshire</h3>
        </div>
+       <div className="sides" id="tel">
+         <p> tel: 07597 852 695</p>
+        
+        </div>
        <div className="sides" id="email">
-   <p > email: cw.plastering.services@gmail.com</p>
+    email: cw.plastering.services@gmail.com
    </div>
        </div>
     )
