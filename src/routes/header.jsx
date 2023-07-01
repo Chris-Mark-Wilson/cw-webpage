dc5ac9cb2d0d1c9332bbc89550a4c4097d87de5f
bigsegs <cw.plastering.services@gmail.com>
2023-07-02 00:03:58 +0100

    added some bumf to header bit pissed

--- src/routes/header.jsx
+++ src/routes/header.jsx
@@ -19,6 +19,22 @@ export const Header=()=>{
         <p>email: cw.plastering.services@gmail.com</p>
         chris@cmwilson.co.uk
         </div>
+        <div>
+            <h6>please excuse this dismal<br/>layout and presenation as I am<br/>(as instructed) concentrating
+            <br/>on the functionality, adding daft ideas<br/>
+            as they occur, and I will, I promise<br/>
+            make this all look pretty.<br/>
+            When, finally, I understand what actually
+            looks impressive to most people,
+            <br/>then, and only then, I might just,<br/> without taking the absolute michael<br/>be capable of earning actual currency..<br/>so, its the 'old guy with a bad back who struggles to knock up 3 ton off the back of the pickup vs the young lad who doesnt know what a ton even looks like, never mind a ton of sharp, soft, ballast or the difference between screed, mortar and concrete, better still how to use all 3 to a professional finish...<br/>... in javascript...'
+            <br/>
+            ps.<br/>
+            I am constantly messing around with this page so if it looks odd one day, it may look even odder the day after, but very soon (relatively) the functionality I was aiming for at the time (eg. global co-ordinates for the weather app) will become apparent.. just because it looks a pig, doesnt mean it is one... <br/>
+            next time you build a house, have a look see that in every single stage its all swept up, painted, plastered etc before they move onto the next stage or do the lads THINK before they crack on... sweep up, tick... think... build... sweep up.... think... build.. etc and then at the last minute.. sweep up, finish, polish, get the actual cleaners in to polish what a builder 'thinks' is polished... <br/>
+            THEN... we start snagging...<br/>
+            just because its a different game, doesn't mean the same rules don't apply...<br/> good luck.. ツ
+            </h6>
+        </div>
    </div>
     )
 }
