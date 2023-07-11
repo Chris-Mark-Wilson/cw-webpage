5d073a5148641042f5111a4f24d6e3faa2bb3721
bigsegs <cw.plastering.services@gmail.com>
2023-07-11 19:53:16 +0100

    adding text for header

--- src/components/geolocation.jsx
+++ src/components/geolocation.jsx
@@ -23,25 +23,34 @@ return (
 
         <label htmlFor="cityName">Search:   </label>
         <input
-        defaultValue="Peterborough"
+        defaultValue=""
           id="cityName"
           type="search"
           variant=""
           name="selectCity"
           style={{ fontSize: "0.8em", width: "55%" }}
-          placeholder="Peterborough"
-          onChange={(element) => setCity(element.target.value)}
+          placeholder="Start typing..."
+          onChange={(element) => {
+            // const event = new Event("change");
+            // const selectElement = document.querySelector('#selectCity')
+            // selectElement.dispatchEvent(event);
+// fires select onChange event to auto load top option into weather
+
+               setCity(element.target.value)
+          
+             }
+          }
         />
       </div>
       <div><label htmlFor="selectCity">Select:</label>
         <select style={{width:'50%', fontSize:'0.8em'}} onChange={(element)=>{
                setLat(element.target.value.split(':')[0])
                setLong(element.target.value.split(':')[1])
-        }}>
+        }} >
               
                 {cityData.map(({name, latitude,longitude,country,admin1,admin2},index)=>{
                     return(
-                        <option key={index} value={`${latitude}:${longitude}`} style={{fontSize:'0.8em'}}>{name},{country},{latitude},{longitude}</option>
+                        <option key={index} value={`${latitude}:${longitude}`} style={{fontSize:'0.8em'}} selected={false}>{name},{country},{latitude},{longitude}</option>
                     )
                     })}
 
